b34febbccb6eae6e4c9721cc91c7b1b473e9f696
Brent Gardner <bgardner@evidation.com>
2024-11-07 12:11:35 -0500

    hacking

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
+    server.listen(8888, err => {
       if (err) throw err;
       console.log(`> Ready on "${config.baseURL}"`);
     });
